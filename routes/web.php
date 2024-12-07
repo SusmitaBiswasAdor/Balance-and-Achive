c504866246b2eaa9b870bb863f50bb63617ba2a5
Susmita Biswas <145971011+SusmitaBiswasAdor@users.noreply.github.com>
2024-12-07 18:49:37 +0600

    Update web.php
    Added the subtask routes-Susmita Biswas

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,19 @@ Route::post('/password/verify', [OtpVerificationController::class, 'verifyOtp'])
 Route::get('/password/reset/{email}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
+//Task routes
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+//Subtask routes
+Route::prefix('tasks/{task}/subtasks')->group(function () {
+    Route::get('/', [SubtaskController::class, 'index'])->name('subtasks.index');
+    Route::post('/', [SubtaskController::class, 'store'])->name('subtasks.store');
+    Route::put('/{subtask}', [SubtaskController::class, 'update'])->name('subtasks.update');
+    Route::delete('/{subtask}', [SubtaskController::class, 'destroy'])->name('subtasks.destroy');
+    Route::get('/manage', [SubtaskController::class, 'manage'])->name('subtasks.manage');
+});
